7dd403e9165ad60009c092b59216cc287daaaec2
flatkat <flatkat181@gmail.com>
2024-07-02 19:59:25 +0200

    Initial commit

---
+++ src/main/java/net/flatkat/ancient_forgotten/AncientAndForgottenClient.java
@@ -0,0 +1,11 @@
+package net.flatkat.ancient_forgotten;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AncientAndForgottenClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/flatkat/ancient_forgotten/AncientAndForgottenDataGenerator.java
@@ -0,0 +1,11 @@
+package net.flatkat.ancient_forgotten;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AncientAndForgottenDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
