ea2ce6b74d4e92970576cb86888c18c3e7f5c5fe
flatkat <fernan181pro@gmail.com>
2024-07-19 19:54:22 +0200

    -Partially added the rose (WIP)

--- src/main/java/net/flatkat/ancient_forgotten/AncientAndForgottenClient.java
+++ src/main/java/net/flatkat/ancient_forgotten/AncientAndForgottenClient.java
@@ -1,11 +1,16 @@
 package net.flatkat.ancient_forgotten;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.flatkat.ancient_forgotten.block.ModBlocks;
+import net.minecraft.client.render.RenderLayer;
 
 public class AncientAndForgottenClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ROSE, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.POTTED_ROSE, RenderLayer.getCutout());
 
     }
 }
