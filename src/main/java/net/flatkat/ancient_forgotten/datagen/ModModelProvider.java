b3c97408714609f07cacd39b1a8d735eea50a305
flatkat <flatkat181@gmail.com>
2024-07-20 20:51:17 +0200

    Moved to ModModelProvider datagen and fixed Rose and Potted Rose model -Enabled ModModelProvider datagen. -Removed all item and block models and blockstates from the data folder of the mod, and replaced it with ModModelProvider datagen. -Added rose and potted rose models to ModModelProvider.

--- src/main/java/net/flatkat/ancient_forgotten/datagen/ModModelProvider.java
+++ src/main/java/net/flatkat/ancient_forgotten/datagen/ModModelProvider.java
@@ -1,4 +1,26 @@
 package net.flatkat.ancient_forgotten.datagen;
 
-public class ModModelProvider {
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.flatkat.ancient_forgotten.block.ModBlocks;
+import net.flatkat.ancient_forgotten.item.ModItems;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.UNKNOWN_BLOCK);
+        blockStateModelGenerator.registerFlowerPotPlant(ModBlocks.ROSE, ModBlocks.POTTED_ROSE, BlockStateModelGenerator.TintType.NOT_TINTED);
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        itemModelGenerator.register(ModItems.RUBY, Models.GENERATED);
+    }
 }
